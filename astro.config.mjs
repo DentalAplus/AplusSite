6086709e39d3d25ff698a31ce537a58f99b0e644
DentalAplus <mirez@yuceler.com>
2026-03-15 23:05:47 +0300

    Sitemap ve GTM eklendi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap'; // 1. Sitemap'i içe aktarıyoruz
 
 // https://astro.build/config
 export default defineConfig({
+  // 2. Kendi alan adını buraya ekliyoruz (Sitemap linkleri için şart)
+  site: 'https://www.dentalimplanttrakya.com', 
+  
+  integrations: [
+    sitemap(), // 3. Entegrasyonu çalıştırıyoruz
+  ],
+  
   vite: {
     plugins: [tailwindcss()]
   }
